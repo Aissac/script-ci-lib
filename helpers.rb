24b4176bbc83baec044c17481679e9c07c16bcdb
Istvan Hoka <istvan.hoka@gmail.com>
2012-09-02 18:31:43 +0300

    Merged changes from script-ci-lib
    Conflicts:
    	script/ci/lib/functions
    	script/ci/lib/helpers.rb

--- helpers.rb
+++ helpers.rb
@@ -8,8 +8,12 @@ module CI
   class ConfigHelper
     attr_reader :rails_root, :ci_env
 
-    def self.setup!(rails_root, ci_env)
-      new(rails_root, ci_env).setup!
+    DEFAULT_CONFIG_PATHS = %W[
+      config/environments/test.rb
+    ]
+
+    def self.setup!(rails_root, ci_env, config_paths=DEFAULT_CONFIG_PATHS)
+      new(rails_root, ci_env, config_paths).setup!
     end
 
     class ConfigTemplate
@@ -31,9 +35,10 @@ module CI
       end
     end
 
-    def initialize(rails_root, ci_env)
+    def initialize(rails_root, ci_env, config_paths)
       @rails_root = rails_root
       @ci_env = ci_env
+      @config_paths = config_paths
     end
 
     def setup!
@@ -42,9 +47,14 @@ module CI
     end
 
     def setup_environment!
-      test_config = "#{rails_root}/config/environments/test.rb"
-      env_config  = "#{rails_root}/config/environments/#{ci_env}.rb"
-      FileUtils.ln_s(test_config, env_config) unless 'test' == ci_env
+      return if 'test' == ci_env
+
+      @config_paths.each do |path|
+        test_config = File.join(rails_root, path)
+        env_config = destination_config_path(path)
+        FileUtils.ln_s(test_config, env_config) 
+      end
+      
     end
 
     def setup_configs!
@@ -58,6 +68,18 @@ module CI
     end
 
     private
+      def destination_config_path(path)
+        source = File.expand_path File.join(rails_root, path)
+        name = if File.directory?(source)
+          ci_env
+        else
+          ext = File.extname(path)
+          "#{ci_env}#{ext}"
+        end
+
+        File.join(File.dirname(source), name)
+      end
+
       def destination_config(template)
         File.join(rails_root, 'config', File.basename(template).gsub(/\.erb$/, ''))
       end
